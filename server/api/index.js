7cdb055577fb8a805b5684b2b3159ac122548e77
rangerx02 <gean.9720@gmail.com>
2025-09-14 18:20:21 -0500

    Arreglos en Server Problemas con vercel

--- server/api/index.js
+++ server/api/index.js
@@ -3,31 +3,24 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 
-//importamos a connectDB
-const {connectDB} = require("./config/database");
-//Importamos el router principal en index.js
+// Importamos a connectDB
+const { connectDB } = require("./config/database");
+// Importamos el router principal en index.js
 const routes = require("./routes/routes.js");
 
 const app = express();
 
-//Configuramos los middleware para que pueda interpretar JSON y habilitar cors
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(cors())
 
-//Creacion de esta parte para evitar restricciones con vercel
+// Configuración de CORS
 const allowedOrigins = [
   process.env.FRONTEND_URL || "https://code-nova-project.vercel.app",
   "http://localhost:5173",
   "http://localhost:3000",
 ];
 
-
-
-
-
-//Creacion de constante para cors
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
@@ -43,20 +36,15 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-
-// Connect to MongoDB - invocamos a la función para conectarnos a la DB
+// Conexión a MongoDB
 connectDB();
 
-
-// Mount routes bajo un prefijo comun
+// Rutas
 app.use("/api/v1", routes);
 
-
-//Creemos una ruta básica--esto quitarlo ¡¡¡¡Leer¡¡
-// Root route
+// Ruta básica
 app.get("/", (req, res) => res.send("Server is running"));
 
-//Creemos nuestro puerto de escucha en una la variable de entorno o el puerto 3000
 // Start server
 if (require.main === module) {
   const PORT = process.env.PORT || 3000;
@@ -65,3 +53,4 @@ if (require.main === module) {
   });
 }
 module.exports = app;
+
